81e6ca8800cfdb539ee8404b21ac68a2c71c2683
jilenski <jtayhopon@gmail.com>
2023-05-12 17:29:56 +0800

    add function types

---
+++ conditionals.js
@@ -0,0 +1,36 @@
+const x = 300;
+
+//instead of using "else if statement" we can use "||" which means "OR"
+if (x === 100 || x > 200) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+/*
+const x = 300;
+
+if (x === 100) {
+  console.log("it's 100");
+} else if (x > 200) {
+  console.log("value is greater than 100");
+} else {
+  console.log("not 100");
+}
+*/
+
+/*
+const x = "100";
+
+if (x == 100) {
+  console.log("it's 100");
+} else {
+  console.log("Not 100");
+} // will return true because "==" only checks the value
+
+if (x === 100) {
+  console.log("it's 100");
+} else {
+  console.log("Not 100");
+} // will return false because "===" checks the value and data type
+*/
