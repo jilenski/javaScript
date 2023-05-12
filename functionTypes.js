23b0c1ae85de3726ee7ceafeaff1d857d5a9351d
jilenski <jtayhopon@gmail.com>
2023-05-12 17:46:43 +0800

    add function types

---
+++ functionTypes.js
@@ -0,0 +1,33 @@
+//simpler way to write the code below using "Arrow Function"
+const getSum = (x, y) => x + y;
+
+console.log(getSum(100, 200));
+
+/*
+//writing the codes below using "Arrow Function"
+const getSum = (x, y) => {
+  return x + y;
+};
+
+console.log(getSum(100, 200));
+*/
+
+/*
+//simpler way to write the code below
+function getSum(x, y) {
+  return x + y;
+}
+
+console.log(getSum(100, 200));
+*/
+
+/*
+let x = 100;
+let y = 200;
+
+function getSum(x, y) {
+  return x + y;
+}
+
+console.log(getSum(x, y));
+*/
